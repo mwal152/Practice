a84d8a68b06419a4af248248084fa304baf2792b
Matt Walden <matthew.walden@bioclinica.com>
2020-08-13 16:48:43 +0100

    Removed option for user to restart

--- bikeshare.py
+++ bikeshare.py
@@ -267,8 +267,6 @@ def main():
         station_stats(df)
         trip_duration_stats(df)
         user_stats(df)
-        restart = input('\nWould you like to restart? Enter yes or no.\n')
-        if restart.lower() != 'yes':
             break
 
 
